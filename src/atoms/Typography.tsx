601b49675fa168c5cca30dbf7181b5c43fa8cdfd
touka1037 <touka1037@gmail.com>
2021-04-26 09:27:32 +0900

    add Typography component

--- src/atoms/Typography.tsx
+++ src/atoms/Typography.tsx
@@ -1,12 +1,16 @@
 import React, {ReactNode} from 'react'
 import styled from "styled-components";
-import {layout, LayoutProps, typography, TypographyProps} from "styled-system";
+import {layout, LayoutProps, typography, TypographyProps, variant} from "styled-system";
 
 export interface Props {
     /**
      * component
      */
     component?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+    /**
+     * variant
+     */
+    variant?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
     /**
      * children
      */
@@ -23,8 +27,46 @@ export interface Props {
 
 const Typography: React.FC<Props> = (props) => {
     return (
-        <StyledTypography as={props.component} display={props.display} textAlign={props.align}>{props.children}</StyledTypography>
+        <StyledTypography as={props.component}
+                          display={props.display}
+                          textAlign={props.align}
+                          variant={props.variant}>{props.children}</StyledTypography>
     )
+}
+
+const variants = {
+    h1: {
+        fontSize: "48px",
+        lineHeight: 1,
+    },
+    h2: {
+        fontSize: "36px",
+        lineHeight: 1.333,
+    },
+    h3: {
+        fontSize: "24px",
+        lineHeight: 1,
+    },
+    h4: {
+        fontSize: "16px",
+        lineHeight: 1,
+    },
+    h5: {
+        fontSize: "16px",
+        lineHeight: 1,
+    },
+    h6: {
+        fontSize: "16px",
+        lineHeight: 1,
+    },
+    p: {
+        fontSize: "16px",
+        lineHeight: 1,
+    }
+}
+
+interface StyledTypographyProps {
+    variant?: string,
 }
 
 Typography.defaultProps = {
@@ -33,9 +75,10 @@ Typography.defaultProps = {
     display: "initial",
 }
 
-const StyledTypography = styled.p<LayoutProps & TypographyProps>`
+const StyledTypography = styled.p<LayoutProps & TypographyProps & StyledTypographyProps>`
   ${typography}
   ${layout}
+  ${variant({variants})}
 `
 
 export default Typography
